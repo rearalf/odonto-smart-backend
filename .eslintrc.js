85eb597834aa71db86c22a655f1d9e2f4951bca1
Ricardo Alf <rear_recinos@hotmail.com>
2025-01-10 15:02:35 -0600

    refactor: Add husky and commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,16 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: { regex: '^I[A-Z]', match: true },
+      },
+    ],
   },
 };
