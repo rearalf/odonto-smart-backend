9ba276473b42983ce6a2964dbb0d2c77d4f7617c
Ricardo Alf <rear_recinos@hotmail.com>
2025-01-13 08:52:06 -0600

    feat: Add swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.setGlobalPrefix(`api`);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Odonto Smart')
+    .setDescription('This is the documentation about a odonto smart.')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
