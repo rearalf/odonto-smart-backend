f98a8587bf4dd7fda6a1ed896b110abe1fdea1d3
Ricardo Alf <rear_recinos@hotmail.com>
2025-01-31 14:01:31 -0600

    fix: Add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,13 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
   app.use(cookieParser());
-  app.enableCors({ credentials: true });
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
 
   app.setGlobalPrefix(`api`);
 
