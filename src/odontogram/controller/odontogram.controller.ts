a721637a34e4f3486fdf8035e5922e80922ee5b1
Ricardo Alf <rear_recinos@hotmail.com>
2025-09-26 14:03:39 -0600

    feat: Add OdontogramSchema for the documentation swagger

--- src/odontogram/controller/odontogram.controller.ts
+++ src/odontogram/controller/odontogram.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { OdontogramService } from '../services/odontogram.service';
 import { Odontogram } from '../entities/odontogram.entity';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OdontogramSchema } from '../schemas/odontrogram.schema';
 
 // import { CreateOdontogramDto } from '../dto/create-odontogram.dto';
 // import { UpdateOdontogramDto } from '../dto/update-odontogram.dto';
@@ -22,7 +23,7 @@ export class OdontogramController {
   @ApiResponse({
     status: 200,
     description: 'The general odontogram for the patient',
-    // type: Odontogram,
+    type: OdontogramSchema,
   })
   @ApiResponse({ status: 404, description: 'Odontogram not found' })
   async getGeneralOdontogram(
@@ -41,7 +42,7 @@ export class OdontogramController {
   @ApiResponse({
     status: 200,
     description: 'The odontogram for the appointment',
-    // type: Odontogram,
+    // type: OdontogramSchema,
   })
   @ApiResponse({ status: 404, description: 'Odontogram not found' })
   async getAppointmentOdontogram(
---
+++ src/odontogram/schemas/odontrogram.schema.ts
@@ -0,0 +1,66 @@
+import { TOOTH_FACE_AFFECTION, TOOTH_STATE } from '@/common/enums/tooth.enum';
+import { ApiProperty } from '@nestjs/swagger';
+
+class ToothSchema {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 1 })
+  odontogram_id: number;
+
+  @ApiProperty({ example: 11 })
+  tooth_number: number;
+
+  @ApiProperty({ enum: TOOTH_STATE, example: TOOTH_STATE.HEALTHY })
+  general_state: TOOTH_STATE;
+
+  @ApiProperty({
+    enum: TOOTH_FACE_AFFECTION,
+    example: TOOTH_FACE_AFFECTION.HEALTHY,
+  })
+  palatina: TOOTH_FACE_AFFECTION;
+
+  @ApiProperty({
+    enum: TOOTH_FACE_AFFECTION,
+    example: TOOTH_FACE_AFFECTION.HEALTHY,
+  })
+  distal: TOOTH_FACE_AFFECTION;
+
+  @ApiProperty({
+    enum: TOOTH_FACE_AFFECTION,
+    example: TOOTH_FACE_AFFECTION.HEALTHY,
+  })
+  mesial: TOOTH_FACE_AFFECTION;
+
+  @ApiProperty({
+    enum: TOOTH_FACE_AFFECTION,
+    example: TOOTH_FACE_AFFECTION.HEALTHY,
+  })
+  vestibular: TOOTH_FACE_AFFECTION;
+
+  @ApiProperty({
+    enum: TOOTH_FACE_AFFECTION,
+    example: TOOTH_FACE_AFFECTION.HEALTHY,
+  })
+  oclusal: TOOTH_FACE_AFFECTION;
+}
+
+export class OdontogramSchema {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 123 })
+  patient_id: number;
+
+  @ApiProperty({ example: 456 })
+  appointment_id: number;
+
+  @ApiProperty({ example: '2025-09-25T17:00:00.000Z', format: 'date-time' })
+  created_at: string;
+
+  @ApiProperty({ example: '2025-09-25T17:00:00.000Z', format: 'date-time' })
+  updated_at: string;
+
+  @ApiProperty({ type: () => [ToothSchema] })
+  tooth: ToothSchema[];
+}
